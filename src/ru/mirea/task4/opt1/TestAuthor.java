c5372e1c4cb2474c3bd16ee79bf9bae53c95c44d
IvanovMirea <tekapzdabrat@gmail.com>
2021-09-29 01:35:15 +0300

    Opt1

---
+++ src/ru/mirea/task4/opt1/TestAuthor.java
@@ -0,0 +1,9 @@
+package ru.mirea.task4.opt1;
+
+public class TestAuthor {
+    public static void main(String[] args){
+        Author author = new Author("Jamie","jamieTheWriter@gmail.com",'m');
+        author.toString();
+        System.out.println(author);
+    }
+}
