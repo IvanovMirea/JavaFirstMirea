0b3a4db89a2d112cce578bc8f15ae85cd41109b9
IvanovMirea <tekapzdabrat@gmail.com>
2021-09-28 23:39:29 +0300

    Opt4

---
+++ src/ru/mirea/task1/opt4/Cikl.java
@@ -0,0 +1,9 @@
+package ru.mirea.task1.opt4;
+
+public class Cikl {
+    public static void main(String[] args) {
+        for (String str : args) {
+            System.out.println("Arg is " +str);
+        }
+    }
+}
