517df08b02f7f2f1ae65ad6305c4cf6d979a3a2a
IvanovMirea <tekapzdabrat@gmail.com>
2021-10-12 02:59:15 +0300

    Opt2

---
+++ src/ru/mirea/task12/opt2/Comparator.java
@@ -0,0 +1,9 @@
+package ru.mirea.task12.opt2;
+
+public interface Comparator {
+    double[] arrSort(double[] arr, int left, int right);
+}
+
+
+
+
---
+++ src/ru/mirea/task12/opt2/Students.java
@@ -0,0 +1,14 @@
+package ru.mirea.task12.opt2;
+
+class Students
+{
+    private double GPA;
+    public Students(double GPA) {
+        this.GPA = GPA;
+    }
+
+    public double getGPA()
+    {
+        return GPA;
+    }
+}
